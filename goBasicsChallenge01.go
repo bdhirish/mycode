93433721eecf03de78a65cb8cc4ba25ecc5236b7
Dhirish <bdhirish@gmail.com>
2021-09-20 17:54:03 +0000

    Learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,13 @@
+package  main
+
+import "fmt"
+
+func main() {
+
+var name,state,city  = "DhirishKumar","Telangana","Hyderabad"
+
+fmt.Println("My Name is :", name)
+fmt.Println("State :" , state)
+
+fmt.Println("City : " , city)
+}
